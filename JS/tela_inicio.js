8f97c6cb5cc78be915555ad30a6b4505bdcc67dc
Geilson <geilsonsrz@gmail.com>
2024-07-03 11:11:22 -0300

    Finalizacao da estruturas e efeitos TELA1

--- JS/tela_inicio.js
+++ JS/tela_inicio.js
@@ -1,19 +1,18 @@
-
-const subtitulo_efeito = document.querySelector('.textoSecundario')
+// Identificação do texto de efeito
+const subtitulo_efeito = document.querySelector('#textoSecundario')
 
 
 const efeitoSubtitulo = () => {
-
+    
     setTimeout(()=>{
-
+        // Adicionando clase de efeito
+        subtitulo_efeito.classList.add('textoSecundario')
         subtitulo_efeito.textContent = 'EM MOVIMENTO'
-        
+
     }, 0)
 
     setTimeout(()=>{
-
         subtitulo_efeito.textContent = 'E SEUS CAMINHOS'
-        
     }, 4000)
 
 }
