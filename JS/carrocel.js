21c22a02197333182e926d8f5e2ccc2d490ba89d
Geilson <geilsonsrz@gmail.com>
2024-07-04 12:36:38 -0300

    Conclusao da estrutura do carrocel

---
+++ JS/carrocel.js
@@ -0,0 +1,17 @@
+// Ativação constante
+setInterval(()=>{
+
+    // Itens do card do carrocel
+    let itens = document.querySelectorAll('.apoiador')
+
+    // Área apoiadores
+    document.querySelector('.area-apoiadores').appendChild(itens[0])
+
+}, 2000)
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -4,4 +4,6 @@ import './JS/navbar.js'
 // Tela Inicial
 import './JS/tela_inicio.js'
 
+// Carrocel
+import './JS/carrocel.js'
 
